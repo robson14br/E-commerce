34dcdfb677a523b7d9f30b0976a79c26a45be9d3
Robson <robson14br@outlook.com>
2024-07-30 19:49:42 -0300

    Feat loading Skeleton

--- next-commerce/src/app/page.tsx
+++ next-commerce/src/app/page.tsx
@@ -1,30 +1,49 @@
 import Image from "next/image";
 import { ProductType } from "@/types/ProductType";
 import Product from "./components/Product";
+import Stripe from "stripe";
 
-async function getProducts() {
-  const res = await fetch("https://fakestoreapi.com/products");
+async function getProducts(): Promise<ProductType[]> {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: "2024-06-20",
+  });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+  const products = await stripe.products.list();
+  const formatedProducts = await Promise.all(
+    products.data.map(async (product) => {
+      const price = await stripe.prices.list({
+        product: product.id,
+      });
+      return {
+        id: product.id,
+        price: price.data[0].unit_amount,
+        name: product.name,
+        image: product.images[0],
+        description: product.description,
+        currency: price.data[0].currency,
+      };
+    })
+  );
 
-  return res.json();
+  return formatedProducts;
+  //const res = await fetch("https://fakestoreapi.com/products");
+
+  //if (!res.ok) {
+  //  throw new Error("Failed to fetch data");
+  //}
+
+  //return res.json();
 }
 
 export default async function Home() {
-const products = await getProducts(); 
+  const products = await getProducts();
 
   return (
     <div className="max-w-7x1 mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
-       
-      {products.map((product: ProductType) => (
-        <Product key={product.id} product={product}>
-          
-        </Product>
-      ))}
-
+        {products.map((product) => (
+          <Product key={product.id} product={product}></Product>
+        ))}
       </div>
     </div>
   );
--- next-commerce/src/middleware.ts
+++ next-commerce/src/middleware.ts
@@ -1,8 +1,14 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-    
-});
+const isPublicRoute = createRouteMatcher([
+ '/', '/product(.*)'
+]);
+
+export default clerkMiddleware((auth, request) =>{
+    if(!isPublicRoute(request)){
+      auth().protect();
+    }
+  });
 
 export const config = {
   matcher: [
