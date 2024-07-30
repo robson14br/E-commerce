59cc8dda63cde1d33776256bbc393dcf907ae090
Robson <robson14br@outlook.com>
2024-07-30 11:08:24 -0300

    feat  cards pt 1

--- next-commerce/src/app/page.tsx
+++ next-commerce/src/app/page.tsx
@@ -2,8 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Next ecommerce</h1>
+    <div className="max-w-7x1 mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+      
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        prot 1
+      </div>
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        prot 1
+      </div>
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        prot 1
+      </div>
+      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+        prot 1
+      </div>
+      </div>
+      
     </div>
   );
 }
